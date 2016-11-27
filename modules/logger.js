15acf5cfc6d1a59e8217e00a482dfa408924e0c5
chiru7 <nagato3c+github@gmail.com>
2016-11-27 13:38:14 +0900

    sequelizeでのsql実行ログについてもlog4jsで出力するように対応。

--- modules/logger.js
+++ modules/logger.js
@@ -4,6 +4,7 @@ log4js.configure('./config/log4js.json');
 
 module.exports = {
   access: log4js.getLogger('access'),
+  sequelize: log4js.getLogger('sequelize'),
   system: log4js.getLogger('system'),
   app: log4js.getLogger('app'),
   express: log4js.connectLogger(log4js.getLogger('access'), {level: log4js.levels.INFO})
